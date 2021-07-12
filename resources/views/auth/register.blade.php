5ede1ecd06c468a69fc199d2a1a2ee1eaec47fb7
Sinmiloluwa <mofeoluwae@gmail.com>
2021-07-12 07:57:46 -0700

    Modified register interface

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,6 +1,196 @@
-@extends('layouts.app')
-
-@section('content')
+<!doctype html>
+<html class="no-js" lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+        <title>Ezone - eCommerce HTML5 Template</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- Favicon -->
+        <link rel="shortcut icon" type="image/x-icon" href="assets/img/favicon.png">
+		
+		<!-- all css here -->
+        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/css/magnific-popup.css">
+        <link rel="stylesheet" href="assets/css/animate.css">
+        <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+        <link rel="stylesheet" href="assets/css/themify-icons.css">
+        <link rel="stylesheet" href="assets/css/pe-icon-7-stroke.css">
+        <link rel="stylesheet" href="assets/css/icofont.css">
+        <link rel="stylesheet" href="assets/css/meanmenu.min.css">
+        <link rel="stylesheet" href="assets/css/bundle.css">
+        <link rel="stylesheet" href="assets/css/style.css">
+        <link rel="stylesheet" href="assets/css/responsive.css">
+        <script src="assets/js/vendor/modernizr-2.8.3.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 8]>
+        <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+        <!-- header start -->
+        <header>
+            <div class="header-top-furniture wrapper-padding-2 res-header-sm">
+                <div class="container-fluid">
+                    <div class="header-bottom-wrapper">
+                        <div class="logo-2 furniture-logo ptb-30">
+                            <a href="{{route('home')}}">
+                                <img src="assets/img/logo/2.png" alt="">
+                            </a>
+                        </div>
+                        <div class="menu-style-2 furniture-menu menu-hover">
+                            <nav>
+                                <ul>
+                                    <li><a href="{{route('home')}}">home</a>
+                                        <ul class="single-dropdown">
+                                            <li><a href="index.html">Fashion</a></li>
+                                            <li><a href="index-fashion-2.html">Fashion style 2</a></li>
+                                            <li><a href="index-fruits.html">fruits</a></li>
+                                            <li><a href="index-book.html">book</a></li>
+                                            <li><a href="index-electronics.html">electronics</a></li>
+                                            <li><a href="index-electronics-2.html">electronics style 2</a></li>
+                                            <li><a href="index-food.html">food & drink</a></li>
+                                            <li><a href="index-furniture.html">furniture</a></li>
+                                            <li><a href="index-handicraft.html">handicraft</a></li>
+                                            <li><a target="_blank" href="index-smart-watch.html">smart watch</a></li>
+                                            <li><a href="index-sports.html">sports</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="#">pages</a>
+                                        <ul class="single-dropdown">
+                                            <li><a href="about-us.html">about us</a></li>
+                                            <li><a href="menu-list.html">menu list</a></li>
+                                            <li><a href="login.html">login</a></li>
+                                            <li><a href="register.html">register</a></li>
+                                            <li><a href="cart.html">cart page</a></li>
+                                            <li><a href="checkout.html">checkout</a></li>
+                                            <li><a href="wishlist.html">wishlist</a></li>
+                                            <li><a href="contact.html">contact</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="shop.html">shop</a>
+                                        <div class="category-menu-dropdown shop-menu">
+                                            <div class="category-dropdown-style category-common2 mb-30">
+                                                <h4 class="categories-subtitle"> shop layout</h4>
+                                                <ul>
+                                                    <li><a href="shop-grid-2-col.html"> grid 2 column</a></li>
+                                                    <li><a href="shop-grid-3-col.html"> grid 3 column</a></li>
+                                                    <li><a href="shop.html">grid 4 column</a></li>
+                                                    <li><a href="shop-grid-box.html">grid box style</a></li>
+                                                    <li><a href="shop-list-1-col.html"> list 1 column</a></li>
+                                                    <li><a href="shop-list-2-col.html">list 2 column</a></li>
+                                                    <li><a href="shop-list-box.html">list box style</a></li>
+                                                    <li><a href="cart.html">shopping cart</a></li>
+                                                    <li><a href="wishlist.html">wishlist</a></li>
+                                                </ul>
+                                            </div>
+                                            <div class="category-dropdown-style category-common2 mb-30">
+                                                <h4 class="categories-subtitle"> product details</h4>
+                                                <ul>
+                                                    <li><a href="product-details.html">tab style 1</a></li>
+                                                    <li><a href="product-details-2.html">tab style 2</a></li>
+                                                    <li><a href="product-details-3.html"> tab style 3</a></li>
+                                                    <li><a href="product-details-4.html">sticky style</a></li>
+                                                    <li><a href="product-details-5.html">sticky style 2</a></li>
+                                                    <li><a href="product-details-6.html">gallery style</a></li>
+                                                    <li><a href="product-details-7.html">gallery style 2</a></li>
+                                                    <li><a href="product-details-8.html">fixed image style</a></li>
+                                                    <li><a href="product-details-9.html">fixed image style 2</a></li>
+                                                </ul>
+                                            </div>
+                                            <div class="mega-banner-img">
+                                                <a href="single-product.html">
+                                                    <img src="assets/img/banner/18.jpg" alt="">
+                                                </a>
+                                            </div>
+                                        </div>
+                                    </li>
+                                    <li><a href="blog.html">blog</a>
+                                        <ul class="single-dropdown">
+                                            <li><a href="blog.html">blog 3 colunm</a></li>
+                                            <li><a href="blog-2-col.html">blog 2 colunm</a></li>
+                                            <li><a href="blog-sidebar.html">blog sidebar</a></li>
+                                            <li><a href="blog-details.html">blog details</a></li>
+                                            <li><a href="blog-details-sidebar.html">blog details 2</a></li>
+                                        </ul>
+                                    </li>
+                                    <li><a href="contact.html">contact</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                        
+                    </div>
+                    <div class="row">
+                        <div class="mobile-menu-area d-md-block col-md-12 col-lg-12 col-12 d-lg-none d-xl-none">
+                            <div class="mobile-menu">
+                                <nav id="mobile-menu-active">
+                                    <ul class="menu-overflow">
+                                        <li><a href="#">HOME</a>
+                                            <ul>
+                                                <li><a href="index.html">Fashion</a></li>
+                                                <li><a href="index-fashion-2.html">Fashion style 2</a></li>
+                                                <li><a href="index-fruits.html">Fruits</a></li>
+                                                <li><a href="index-book.html">book</a></li>
+                                                <li><a href="index-electronics.html">electronics</a></li>
+                                                <li><a href="index-electronics-2.html">electronics style 2</a></li>
+                                                <li><a href="index-food.html">food & drink</a></li>
+                                                <li><a href="index-furniture.html">furniture</a></li>
+                                                <li><a href="index-handicraft.html">handicraft</a></li>
+                                                <li><a href="index-smart-watch.html">smart watch</a></li>
+                                                <li><a href="index-sports.html">sports</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="#">pages</a>
+                                            <ul>
+                                                <li><a href="about-us.html">about us</a></li>
+                                                <li><a href="menu-list.html">menu list</a></li>
+                                                <li><a href="login.html">login</a></li>
+                                                <li><a href="register.html">register</a></li>
+                                                <li><a href="cart.html">cart page</a></li>
+                                                <li><a href="checkout.html">checkout</a></li>
+                                                <li><a href="wishlist.html">wishlist</a></li>
+                                                <li><a href="contact.html">contact</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="#">shop</a>
+                                            <ul>
+                                                <li><a href="shop-grid-2-col.html"> grid 2 column</a></li>
+                                                <li><a href="shop-grid-3-col.html"> grid 3 column</a></li>
+                                                <li><a href="shop.html">grid 4 column</a></li>
+                                                <li><a href="shop-grid-box.html">grid box style</a></li>
+                                                <li><a href="shop-list-1-col.html"> list 1 column</a></li>
+                                                <li><a href="shop-list-2-col.html">list 2 column</a></li>
+                                                <li><a href="shop-list-box.html">list box style</a></li>
+                                                <li><a href="product-details.html">tab style 1</a></li>
+                                                <li><a href="product-details-2.html">tab style 2</a></li>
+                                                <li><a href="product-details-3.html"> tab style 3</a></li>
+                                                <li><a href="product-details-4.html">sticky style</a></li>
+                                                <li><a href="product-details-5.html">sticky style 2</a></li>
+                                                <li><a href="product-details-6.html">gallery style</a></li>
+                                                <li><a href="product-details-7.html">gallery style 2</a></li>
+                                                <li><a href="product-details-8.html">fixed image style</a></li>
+                                                <li><a href="product-details-9.html">fixed image style 2</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="#">BLOG</a>
+                                            <ul>
+                                                <li><a href="blog.html">blog 3 colunm</a></li>
+                                                <li><a href="blog-2-col.html">blog 2 colunm</a></li>
+                                                <li><a href="blog-sidebar.html">blog sidebar</a></li>
+                                                <li><a href="blog-details.html">blog details</a></li>
+                                                <li><a href="blog-details-sidebar.html">blog details 2</a></li>
+                                            </ul>
+                                        </li>
+                                        <li><a href="contact.html"> Contact  </a></li>
+                                    </ul>
+                                </nav>							
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        </header>
+        <div class="register-area ptb-100">
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -63,15 +253,19 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-secondary">
                                     {{ __('Register') }}
                                 </button>
                             </div>
+                            
                         </div>
                     </form>
+                    <div class="col-md-6 ">
+                                  <a href="{{route('login')}}">{{__('Login')}}</a>
+                            </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
-@endsection
+
