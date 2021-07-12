4c7735cbeed04656051934a53261b0855be0059b
Sinmiloluwa <mofeoluwae@gmail.com>
2021-07-12 07:58:20 -0700

    Added banner

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -108,10 +108,35 @@
                             </button>
                         </form>
                     </div>
+                   
                     <div class="header-login">
                         <ul>
-                            <li><a href="login.html">Login</a></li>
+                        @guest
+                        @if (Route::has('login'))
+                            <li><a href="{{route('login')}}">Login</a></li>
+                            @endif
+                            @if (Route::has('register'))
                             <li><a href="register.html">Reg</a></li>
+                            @endif
+                            @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
                         </ul>
                     </div>
                     <div class="header-cart cart-res">
