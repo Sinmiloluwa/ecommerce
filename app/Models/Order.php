7c39877ebfda5a805829a35290a3dab078a9eeb9
Sinmiloluwa <mofeoluwae@gmail.com>
2021-07-19 02:53:10 -0700

    Added transactions

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -3,10 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Model; 
 
 class Order extends Model
 {
+    protected $guarded =[];
     use HasFactory;
 
     public function items(){
@@ -16,4 +17,31 @@ class Order extends Model
     public function user(){
         return $this->belongsTo(User::class);
     }
+
+    public function subOrders()
+    {
+        return $this->hasMany(SubOrder::class); 
+    }
+
+    public function generateSubOrders()
+    {
+        $orderItems = $this->items;
+
+        foreach($orderItems->groupBy('shop_id') as $shopId => $products) {
+            $shop = Shop::find($shopId);
+
+            $subOrder = $this->subOrders()->create([
+                'order_id' => $this->id,
+                'seller_id' => $shop->user_id,
+                'grand_total' => $products->sum('pivot.price'),
+                'item_count' => $products->count()
+            ]);
+        }
+
+        foreach($products as $product) {
+            $subOrder->items()->attach($product->id, ['price' => $product->pivot->price, 'quantity' => $product->pivot->quantity]);
+        }
+    }
+
+    
 }
