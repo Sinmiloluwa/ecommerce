16c816ee929795a6ecb98054f67697e099c2f27f
Sinmiloluwa <mofeoluwae@gmail.com>
2021-07-21 09:57:28 -0700

    Added many to many relationship with products

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,13 +4,37 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use TCG\Voyager\Models\Category as ModelsCategory;
 
-class Category extends Model
+class Category extends ModelsCategory
 {
     use HasFactory;
+    public function child()
+    {
+        return $this->hasMany(Category::class, 'parent_id');
+    }
+
+    public function products()
+    {
+        return $this->belongsToMany(Product::class, 'product_categories');
+    }
 
     public function allProducts()
     {
-        return $this->hasMany(Category::class);
+        $allProducts = collect();
+
+        $mainCategoryProducts = $this->products;
+
+        $allProducts = $allProducts->concat($mainCategoryProducts);
+
+        $children = $this->children;
+
+        if ($children) {
+            foreach($this->children as $child){
+                $allProducts = $allProducts->concat($child->products);
+            }
+        }
+
+        return $allProducts;
     }
 }
